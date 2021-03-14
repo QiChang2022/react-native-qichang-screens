610b8448fff7f45d37ba9966445c8a8e76a3579a
Damoness <qjwdcool@qq.com>
2021-03-14 18:56:24 +0800

    fix: export SearchScreen

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,3 @@
 import QichanghaoRankingScreen from './qichanghao-ranking-screen';
-import SearchScreen from './search-screen';
+import * as SearchScreen from './search-screen';
 export { QichanghaoRankingScreen, SearchScreen };
